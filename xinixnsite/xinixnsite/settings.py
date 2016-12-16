8218f1e36620a64c636a5af12cc9a35369d1d5c5
chaochao1987 <549303402@qq.com>
2016-12-16 11:04:31 +0800

    new

--- xinixnsite/xinixnsite/settings.py
+++ xinixnsite/xinixnsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'xinixnsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+        'DIRS': (os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- xinixnsite/xinixnsite/views.py
+++ xinixnsite/xinixnsite/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 import datetime
 from django.http import Http404, HttpResponse
-from django.template.loader import get_template
-from django.template import Template, Context
+from django.shortcuts import render_to_response
+
 
 
 def hello(request):
@@ -15,11 +15,10 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    #html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    
+    return render_to_response('dateapp/headhour.html', {'hour_offset': offset,'next_time': dt})
     
 def current_datetime(request):
-    now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    date = datetime.datetime.now()      
+    return render_to_response('dateapp/current_datetime.html',{'current_date': date})
